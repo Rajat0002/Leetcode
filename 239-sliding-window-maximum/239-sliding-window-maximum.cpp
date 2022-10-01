3307c82dedb20b13a1f4379e8746c50a5aaabac1
Rajat <91206945+Rajat0002@users.noreply.github.com>
2022-10-01 11:20:25 +0530

    Time: 713 ms (23.21%), Space: 134.6 MB (81.59%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+   vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         deque<int> dq;
         vector<int> ans;
         for(int i=0;i<nums.size();i++){
@@ -15,5 +15,42 @@ public:
               ans.push_back(nums[dq.front()]);
         }
         return ans;
+        
+        
+        
+        
+        
+        
+        
+        
+        // vector<int> v;
+        // for(int i=0;i<nums.size()-k;i++){
+        //     int maxn=nums[i];
+        //     for(int j=i;j<i+k;j++){
+        //         v.push_back(max(nums[j],maxn));
+        //     }
+        // }
+        // return v;
+       
+    
+//     void GetMax(vector < int > nums, int l, int r, vector < int > & arr) {
+//   int i, maxi = INT_MIN;
+//   for (i = l; i <= r; i++)
+//     maxi = max(maxi, nums[i]);
+//   arr.push_back(maxi);
+// }
+// vector < int > maxSlidingWindow(vector < int > & nums, int k) {
+//   int left = 0, right = 0;
+//   int i, j;
+//   vector < int > arr;
+//   while (right < k - 1) {
+//     right++;
+//   }
+//   while (right < nums.size()) {
+//     GetMax(nums, left, right, arr);
+//     left++;
+//     right++;
+//   }
+//   return arr;
     }
 };
