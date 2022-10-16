4b41fe83942b6254ee36f92f142eaa82a9430aae
Rajat <91206945+Rajat0002@users.noreply.github.com>
2022-10-16 20:49:37 +0530

    Time: 12 ms (49.11%), Space: 8.9 MB (38.73%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -2,6 +2,18 @@ class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
         
+        
+        unordered_set<int> data;
+        int ret = 0;
+        for(auto const &i: nums) {
+            data.insert(i);
+            if(data.count(i-diff) && data.count(i-diff*2)){
+                ++ret; 
+            }
+        }
+        return ret;
+    
+        
 //         int n=nums.size();
 //         map<int,int> m;
         
@@ -11,21 +23,23 @@ public:
 //             if()
             
 //         }
-         int ans = 0 ;
-    for(int i = 0 ; i < nums.size()-2 ; i++)
-    {
-        for(int j = i+1 ; j < nums.size()-1 ; j++)
-        {
-            if(nums[i]+diff==nums[j])
-            {
-            for(int k = j +1; k < nums.size(); k++)
-              {
-                if(nums[j]+diff==nums[k])
-                   ans++;
-              }
-            }
-        }
-    }
-    return ans;
+         
+        
+//         int ans = 0 ;
+//     for(int i = 0 ; i < nums.size()-2 ; i++)
+//     {
+//         for(int j = i+1 ; j < nums.size()-1 ; j++)
+//         {
+//             if(nums[i]+diff==nums[j])
+//             {
+//             for(int k = j +1; k < nums.size(); k++)
+//               {
+//                 if(nums[j]+diff==nums[k])
+//                    ans++;
+//               }
+//             }
+//         }
+//     }
+//     return ans;
     }
 };
